3dcd04cd96c910fc408af5da53b5e0b31c7cc6e4
Mathieu <tsunoll96@gmail.com>
2020-11-23 10:20:03 +0100

    Rule
    *Rule class c# contains string in entry and string in exit

---
+++ L-System_Vegetation/Assets/Rule.cs
@@ -0,0 +1,21 @@
+﻿public class Rule
+{
+    private string str_InR;
+    private string str_OutR;
+
+    public Rule(string i, string o)
+    {
+        str_InR = i;
+        str_OutR = o;
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
